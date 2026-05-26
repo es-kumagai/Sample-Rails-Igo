7cf26f409f5d327f18b74ec48db303c820eae4a9
Tomohiro Kumagai <tomo@ez-net.jp>
2026-05-26 16:34:30 +0900

    Use importmap.

---
+++ app/javascript/show.js
@@ -0,0 +1,23 @@
+import "@hotwired/turbo-rails"
+import "controllers"
+
+const playGoBoard = () => {
+	const board = document.getElementById("go-board")
+	const stones = Array.from(board.querySelectorAll("[data-go-move-number]")).sort((a, b) => {
+		return Number(a.dataset.goMoveNumber) - Number(b.dataset.goMoveNumber)
+	})
+
+	const delay = 500
+
+	stones.forEach((stone) => {
+		stone.classList.add("go-stone--hidden")
+	})
+
+	stones.forEach((stone, index) => {
+		setTimeout(() => {
+			stone.classList.remove("go-stone--hidden")
+		}, delay * (index + 1))
+	})
+}
+
+document.addEventListener("turbo:load", playGoBoard)
