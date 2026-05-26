5cc2da48f0dcac34dbaf9cae17e39a9d39086afa
Tomohiro Kumagai <tomo@ez-net.jp>
2026-05-26 17:30:59 +0900

    Add empty system test.

---
+++ test/system/kifus_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class KifusTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit kifus_url
+  #
+  #   assert_selector "h1", text: "Kifu"
+  # end
+end
